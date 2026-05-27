3bf8e09412f3a4d251a047ef03d3bb01b3ceadd2
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-05-28 02:07:27 +0300

    feat: add navbar and enhance & config:add fontawesome

--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -1,9 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FlowbiteService } from '../../../core/services/flowbite.service';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { initFlowbite } from 'flowbite';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
-export class NavbarComponent {}
+export class NavbarComponent implements OnInit {
+  constructor(private flowbiteService: FlowbiteService) {}
+
+  ngOnInit(): void {
+    this.flowbiteService.loadFlowbite((flowbite)=>{
+       initFlowbite()
+    })
+  }
+
+}
