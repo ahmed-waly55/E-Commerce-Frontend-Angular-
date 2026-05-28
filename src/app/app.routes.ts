851857c823a76974e3fa804c52a7455528aa9b90
ahmed-waly55 <ahmedwaly074@gmail.com>
2026-05-28 14:08:17 +0300

    add router link and router link active

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,20 +4,25 @@ import { LoginComponent } from './core/components/login/login.component';
 import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
 import { HomeComponent } from './feature/components/home/home.component';
 import { NotfoundComponent } from './feature/components/notfound/notfound.component';
+import { RegisterComponent } from './core/components/register/register.component';
+import { BrandsComponent } from './feature/components/brands/brands.component';
+import { CardComponent } from './feature/components/cart/card.component';
+import { CategoriesComponent } from './feature/components/categories/categories.component';
+import { ProductComponent } from './feature/components/product/product.component';
 
 export const routes: Routes = [
   {path:"", redirectTo:"home", pathMatch:"full"},
   {path:"", component:AuthLayoutComponent, children:[
     {path:"login", component:LoginComponent, title:"Login"},
-    {path:"register", component:LoginComponent, title:"Register"},
+    {path:"register", component:RegisterComponent, title:"Register"},
 
   ]},
   {path:"", component:MainLayoutComponent, children:[
     {path:"home", component:HomeComponent, title:"Home"},
-    {path:"brands", component:HomeComponent, title:"Brands"},
-    {path:"cart", component:HomeComponent, title:"Cart"},
-    {path:"products", component:HomeComponent, title:"Products"},
-    {path:"categories", component:HomeComponent, title:"Categories"},
+    {path:"brands", component:BrandsComponent, title:"Brands"},
+    {path:"cart", component:CardComponent, title:"Cart"},
+    {path:"products", component:ProductComponent, title:"Products"},
+    {path:"categories", component:CategoriesComponent, title:"Categories"},
   ]},
   {path:"**", component:NotfoundComponent, title:"Not Found 404 "},
 ];
--- src/app/shared/components/navbar/navbar.component.ts
+++ src/app/shared/components/navbar/navbar.component.ts
@@ -2,10 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { FlowbiteService } from '../../../core/services/flowbite.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { initFlowbite } from 'flowbite';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink,RouterLinkActive],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css',
 })
